64e1403b978ddfe87801b3f3ce029c940f6409c5
Isabel Sánchez Castro <isasancas@alum.us.es>
2025-02-23 19:22:17 +0100

    Some forgotten constraints

--- src/main/java/acme/entities/FlightAssignment.java
+++ src/main/java/acme/entities/FlightAssignment.java
@@ -4,8 +4,11 @@ package acme.entities;
 import java.util.Date;
 
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
@@ -19,24 +22,30 @@ public class FlightAssignment extends AbstractEntity {
 
 	@Mandatory
 	@OneToOne
+	@Automapped
 	private FlightCrewMembers	flightCrewMember;
 
 	//@Mandatory
 	//@OneToOne
+	//@Automapped
 	//private Leg leg;
 
 	@Mandatory
+	@Automapped
 	private Enum<Duty>			duty;
 
 	@Mandatory
 	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date				moment;
 
 	@Mandatory
+	@Automapped
 	private Enum<CurrentStatus>	currentStatus;
 
 	@Optional
 	@ValidString
+	@Automapped
 	private String				remarks;
 
 }
--- src/main/java/acme/entities/FlightCrewMembers.java
+++ src/main/java/acme/entities/FlightCrewMembers.java
@@ -6,9 +6,11 @@ import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import lombok.Getter;
 import lombok.Setter;
@@ -29,23 +31,30 @@ public class FlightCrewMembers extends AbstractEntity {
 
 	@Mandatory
 	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
 	private String						phoneNumber;
 
 	@Mandatory
 	@ValidString
+	@Automapped
 	private String						languageSkills;
 
 	@Mandatory
+	@Automapped
 	private Enum<AvailabilityStatus>	availabilityStatus;
 
 	// @Mandatory
+	// @Automapped
 	// private Airlane airlane;
 
 	@Mandatory
 	@ValidMoney
+	@Automapped
 	private Money						salary;
 
 	@Optional
+	@Automapped
+	@ValidNumber(min = 0)
 	private Integer						yearsOfExperience;
 
 }
