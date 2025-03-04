bdbe05c4661246d22d3c0a2b99e497bd104f31b1
Javier Aponte <japontep02@gmail.com>
2025-03-04 11:26:32 +0100

    Added Equals override to the class

--- src/main/java/acme/entities/aircraft/Aircraft.java
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -1,6 +1,8 @@
 
 package acme.entities.aircraft;
 
+import java.util.Objects;
+
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
@@ -53,4 +55,27 @@ public class Aircraft extends AbstractEntity {
 	public enum AircraftStatus {
 		ACTIVE, MAINTENANCE
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(this.capacity, this.cargoWeight, this.details, this.model, this.regNumber, this.status);
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Aircraft other = (Aircraft) obj;
+		return Objects.equals(this.capacity, other.capacity) && Objects.equals(this.cargoWeight, other.cargoWeight) && Objects.equals(this.details, other.details) && Objects.equals(this.model, other.model) && Objects.equals(this.regNumber, other.regNumber)
+			&& this.status == other.status;
+	}
+
 }
