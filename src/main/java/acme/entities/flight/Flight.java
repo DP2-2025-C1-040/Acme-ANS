e1541acfed3536702243348c1446253613219824
Javier Aponte <japontep02@gmail.com>
2025-03-10 23:04:44 +0100

    Updated Flight entity preparing relations before merging

--- src/main/java/acme/entities/flight/Flight.java
+++ src/main/java/acme/entities/flight/Flight.java
@@ -1,10 +1,14 @@
 
 package acme.entities.flight;
 
+import javax.persistence.Transient;
+
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Moment;
 import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 
@@ -32,8 +36,45 @@ public class Flight extends AbstractEntity {
 
 	@Mandatory
 	@ValidString
+	@Automapped
 	private String				description;
+
+	@Mandatory
+	@ValidMoment
+	@Transient
+	private Moment				scheduledDeparture;
+
+	@Mandatory
+	@ValidMoment
+	@Transient
+	private Moment				scheduledArrival;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Transient
+	private String				originCity;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Transient
+	private String				destinationCity;
+
+	// Derived attributes -----------------------------------------------------
+
+	/*
+	 * public Integer getNumberOfLayovers() {
+	 * return legs != null && legs.size() > 1 ? legs.size() - 1 : 0;
+	 * }
+	 */
 
 	// Relationships ----------------------------------------------------------
 
+	/*
+	 * @Mandatory
+	 * 
+	 * @Valid
+	 * 
+	 * @ManyToOne
+	 * private List<Leg> legs;
+	 */
 }
