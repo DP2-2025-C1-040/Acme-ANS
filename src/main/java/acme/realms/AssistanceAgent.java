53bee966056956f90f4288523d24201183062d64
pausualin <pausualin@alum.us.es>
2025-03-12 20:10:22 +0100

    Añadidos validators AssistanceAgent

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,10 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -19,11 +17,12 @@ import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
-import acme.entities.airline.Airline;
+import acme.constraints.ValidAssistanceAgent;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@ValidAssistanceAgent
 @Getter
 @Setter
 public class AssistanceAgent extends AbstractRole {
@@ -67,9 +66,9 @@ public class AssistanceAgent extends AbstractRole {
 	// Derived attributes -------------------------------------------------------------
 
 	// Relationships ------------------------------------------------------------------
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Airline				airline;
+	//@Mandatory
+	//	@Valid
+	//@ManyToOne(optional = false)
+	//private Airline				airline;
 
 }
