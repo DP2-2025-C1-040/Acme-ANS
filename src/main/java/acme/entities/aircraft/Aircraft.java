6deaf6164cc73ea28ad1f8a22e4979da3915ac5d
Javier Aponte <japontep02@gmail.com>
2025-03-12 20:33:31 +0100

    Merged Airline and moved AircraftStatus to a dedicated Enumerated class

--- src/main/java/acme/entities/aircraft/Aircraft.java
+++ src/main/java/acme/entities/aircraft/Aircraft.java
@@ -1,9 +1,9 @@
 
 package acme.entities.aircraft;
 
-import java.util.Objects;
-
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -11,7 +11,13 @@ import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.entities.airline.Airline;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Aircraft extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -43,48 +49,18 @@ public class Aircraft extends AbstractEntity {
 	@Mandatory
 	@Valid
 	@Automapped
-	private AircraftStatus		status;
+	private AircraftStatus		aircraftStatus;
 
 	@Mandatory
 	@ValidString
 	@Automapped
 	private String				details;
 
+	// Relationships ----------------------------------------------------------
 
-	public enum AircraftStatus {
-		ACTIVE, MAINTENANCE
-	}
-
-	//Pendiente de Merge
-	/*
-	 * @Mandatory
-	 * 
-	 * @Valid
-	 * 
-	 * @ManyToOne(optional = false)
-	 * private Airline airline;
-	 */
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(this.capacity, this.cargoWeight, this.details, this.model, this.regNumber, this.status);
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (this.getClass() != obj.getClass())
-			return false;
-		Aircraft other = (Aircraft) obj;
-		return Objects.equals(this.capacity, other.capacity) && Objects.equals(this.cargoWeight, other.cargoWeight) && Objects.equals(this.details, other.details) && Objects.equals(this.model, other.model) && Objects.equals(this.regNumber, other.regNumber)
-			&& this.status == other.status;
-	}
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 
 }
--- src/main/java/acme/entities/airport/Airport.java
+++ src/main/java/acme/entities/airport/Airport.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.airport;
 
 import java.util.Objects;
 
