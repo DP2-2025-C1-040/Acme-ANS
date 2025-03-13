a717c884c23fad10792cc9f262b46059a359f0df
Javier Aponte <japontep02@gmail.com>
2025-03-13 01:04:36 +0100

    Airline missing Getter and Setter annotations

--- src/main/java/acme/entities/airline/Airline.java
+++ src/main/java/acme/entities/airline/Airline.java
@@ -2,9 +2,9 @@
 package acme.entities.airline;
 
 import java.util.Date;
-import java.util.Objects;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -16,7 +16,12 @@ import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Airline extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
@@ -56,27 +61,5 @@ public class Airline extends AbstractEntity {
 	@ValidString(pattern = "^\\+?\\d{6,15}$")
 	@Automapped
 	private String				phoneNumber;
-
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(this.emailAdress, this.foundationMoment, this.iataCode, this.name, this.phoneNumber, this.type, this.webSite);
-		return result;
-	}
-
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (this.getClass() != obj.getClass())
-			return false;
-		Airline other = (Airline) obj;
-		return Objects.equals(this.emailAdress, other.emailAdress) && Objects.equals(this.foundationMoment, other.foundationMoment) && Objects.equals(this.iataCode, other.iataCode) && Objects.equals(this.name, other.name)
-			&& Objects.equals(this.phoneNumber, other.phoneNumber) && Objects.equals(this.type, other.type) && Objects.equals(this.webSite, other.webSite);
-	}
 
 }
