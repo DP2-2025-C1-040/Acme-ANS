52f033973b817edb7a2cb4fb44cd41d6c8b48d68
Isabel Sánchez Castro <isasancas@alum.us.es>
2025-03-13 18:27:17 +0100

    Import fix

--- src/main/java/acme/entities/claims/Claim.java
+++ src/main/java/acme/entities/claims/Claim.java
@@ -13,6 +13,7 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidEmail;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
+import acme.entities.leg.Leg;
 import acme.realms.AssistanceAgent;
 import lombok.Getter;
 import lombok.Setter;
