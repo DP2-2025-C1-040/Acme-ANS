6c58271aa2814b7959a88dd0007526aed11fc983
Isabel Sánchez Castro <isasancas@alum.us.es>
2025-03-13 13:51:42 +0100

    fix flight assignment

--- src/main/java/acme/entities/assignment/FlightAssignment.java
+++ src/main/java/acme/entities/assignment/FlightAssignment.java
@@ -16,6 +16,7 @@ import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidString;
 import acme.entities.crew.FlightCrewMembers;
+import acme.entities.leg.Leg;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -53,9 +54,9 @@ public class FlightAssignment extends AbstractEntity {
 	@Automapped
 	private FlightCrewMembers	flightCrewMember;
 
-	//@Mandatory
-	//@ManyToOne
-	//@Automapped
-	//private Leg leg;
+	@Mandatory
+	@ManyToOne
+	@Automapped
+	private Leg					leg;
 
 }
