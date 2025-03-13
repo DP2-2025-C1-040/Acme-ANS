2d107e31f766a45c8e3314091ca10e4dfd413661
Julia Virginia Angeles Burgos <julangbur@alum.us.es>
2025-03-13 11:49:34 +0100

    fix: add @Entity annotation and enforce attribute ranges

--- src/main/java/acme/entities/tasks/Task.java
+++ src/main/java/acme/entities/tasks/Task.java
@@ -1,13 +1,19 @@
 
 package acme.entities.tasks;
 
+import javax.persistence.Entity;
 import javax.validation.Valid;
 
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
 public class Task {
 
 	// Serialisation version --------------------------------------------------
@@ -32,7 +38,7 @@ public class Task {
 	private Integer				priority;
 
 	@Mandatory
-	@ValidNumber(min = 0)
+	@ValidNumber(min = 0, max = 1000)
 	@Automapped
 	private Integer				estimatedDuration;
 
