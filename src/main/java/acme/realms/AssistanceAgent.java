eb86308d1815a13e97389722d5a03049368c9f69
pausualin <pausualin@alum.us.es>
2025-03-13 12:15:49 +0100

    Descomentar relacion airline-agent

--- src/main/java/acme/realms/AssistanceAgent.java
+++ src/main/java/acme/realms/AssistanceAgent.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractRole;
 import acme.client.components.datatypes.Money;
@@ -66,9 +68,9 @@ public class AssistanceAgent extends AbstractRole {
 	// Derived attributes -------------------------------------------------------------
 
 	// Relationships ------------------------------------------------------------------
-	//@Mandatory
-	//	@Valid
-	//@ManyToOne(optional = false)
-	//private Airline				airline;
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Airline				airline;
 
 }
