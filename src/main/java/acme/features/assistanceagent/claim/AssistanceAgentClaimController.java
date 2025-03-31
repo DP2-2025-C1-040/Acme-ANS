870f4bdf477e5c774c548682886b23a17dd464f7
pausualin <pausualin@alum.us.es>
2025-04-01 01:32:29 +0200

    feat: Added claims list

---
+++ src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimController.java
@@ -0,0 +1,28 @@
+
+package acme.features.assistanceagent.claim;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.claims.Claim;
+import acme.realms.assistanceAgent.AssistanceAgent;
+
+@GuiController
+public class AssistanceAgentClaimController extends AbstractGuiController<AssistanceAgent, Claim> {
+
+	//Internal state --------------------------------------------------------------
+
+	@Autowired
+	private AssistanceAgentClaimListService listService;
+
+	//Constructors ----------------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+	}
+}
---
+++ src/main/java/acme/features/assistanceagent/claim/AssistanceAgentClaimListService.java
@@ -0,0 +1,61 @@
+
+package acme.features.assistanceagent.claim;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.claims.Claim;
+import acme.entities.claims.ClaimStatus;
+import acme.realms.assistanceAgent.AssistanceAgent;
+
+@GuiService
+public class AssistanceAgentClaimListService extends AbstractGuiService<AssistanceAgent, Claim> {
+
+	//Internal state ---------------------------------------------
+
+	@Autowired
+	private AssistanceAgentClaimRepository repository;
+
+	//AbstractGuiService interface -------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Claim> claims;
+		int assistaceAgentId;
+
+		assistaceAgentId = super.getRequest().getPrincipal().getActiveRealm().getId();
+
+		claims = this.repository.findAllClaimsByAssistanceAgentId(assistaceAgentId);
+
+		super.getBuffer().addData(claims);
+
+	}
+
+	@Override
+	public void unbind(final Claim claim) {
+
+		Dataset dataset;
+		ClaimStatus status;
+
+		Boolean publish = claim.getPublished();
+		status = claim.getAccepted();
+
+		dataset = super.unbindObject(claim, "registrationMoment", "passengerEmail", "description", "type");
+		dataset.put("status", status);
+		super.addPayload(dataset, claim, "registrationMoment", "description");
+
+		super.getResponse().addData(dataset);
+
+	}
+
+}
